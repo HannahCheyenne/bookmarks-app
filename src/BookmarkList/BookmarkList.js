4215346b549eb3a1d877ec3757db5921f3c8ec5a
Hannah <HannahCheyenne@gmail.com>
2020-09-08 02:32:28 -0500

    implementing patch

--- src/BookmarkList/BookmarkList.js
+++ src/BookmarkList/BookmarkList.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
+import BookmarksContext from '../BookmarksContext';
 import BookmarkItem from '../BookmarkItem/BookmarkItem';
+import PropTypes from 'prop-types'
 import './BookmarkList.css'
 
 class BookmarkList extends Component {
+  static propTypes = {
+    bookmarks: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.oneOfType([
+          PropTypes.number,
+          PropTypes.string,
+        ]).isRequired,
+      })
+    )
+  }
+
   static defaultProps = {
     bookmarks: []
-  };
+  }
+
+  static contextType= BookmarksContext;
 
   render() {
-    const { bookmarks } = this.props
+    const { bookmarks } = this.context
     return (
       <section className='BookmarkList'>
         <h2>Your bookmarks</h2>
@@ -26,3 +41,12 @@ class BookmarkList extends Component {
 }
 
 export default BookmarkList;
+
+BookmarkList.propTypes = {
+  bookmarks: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    rating: PropTypes.number,
+    description: PropTypes.string
+  }))
+};
